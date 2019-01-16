7230b4b83a02668cd7eb6627effbfe0a8b97d9d9
Shmuel Melamud <smelamud@redhat.com>
2019-01-16 23:04:09 +0200

    Added webextension-polyfill. Now the extension works in Chrome too.

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+import browser from "webextension-polyfill";
+
 function probe(requestDetails) {
     if (requestDetails.responseHeaders) {
         for (let header of requestDetails.responseHeaders) {
