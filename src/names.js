15332a963255c7b6929707981a83a183c57bd663
Shmuel Melamud <smelamud@redhat.com>
2020-12-23 02:30:17 +0200

    Minor release. Purge cached names only when the cache is full.

--- src/names.js
+++ src/names.js
@@ -3,7 +3,6 @@ import browser from 'webextension-polyfill';
 import AsyncLock from "./async-lock";
 import { broadcastMessage, getTabClientUrl } from "./tabs";
 
-const NAME_TTL = 6 * 60 * 60; // seconds
 const MAX_NAMES_SIZE = 500;
 const namesLock = new AsyncLock();
 
@@ -36,10 +35,12 @@ export async function storeName(tabId, details) {
         if (names == null) {
             names = [];
         }
-        const now = Math.round(Date.now() / 1000);
-        names = names.filter(info => info.name !== details.name && (now - info.updated) <= NAME_TTL);
-        details.updated = now;
+        names = names.filter(info => info.name !== details.name);
+        if (details.updated != null) {
+            details.updated = Math.round(Date.now() / 1000);
+        }
         names.push(details);
+        names.sort((a, b) => a.updated - b.updated);
         if (names.length > MAX_NAMES_SIZE) {
             names.splice(0, names.length - MAX_NAMES_SIZE);
         }
